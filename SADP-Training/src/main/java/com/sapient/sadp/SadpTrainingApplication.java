49f5145e0cfa9cc7b1cc61c12bc1c46d9f807423
Suraj Rawat <surajsinghrawat12345@gmail.com>
2017-06-14 16:40:25 +0530

    [Suraj] Initial Commit for project setup

---
+++ SADP-Training/src/main/java/com/sapient/sadp/SadpTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.sapient.sadp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadpTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadpTrainingApplication.class, args);
+	}
+}
